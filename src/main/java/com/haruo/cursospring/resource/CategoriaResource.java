376f454fabbaa4fe0ea462f272df26bc04e75fcf
Haruo Sugano <haruosugano@icloud.com>
2021-08-31 15:28:27 -0300

    Testando a classe dominio

--- src/main/java/com/haruo/cursospring/resource/CategoriaResource.java
+++ src/main/java/com/haruo/cursospring/resource/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.haruo.cursospring.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.haruo.cursospring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		/**
+		 * Retornando em formato JSON
+		 */
+		return lista; 
 	}
 }
